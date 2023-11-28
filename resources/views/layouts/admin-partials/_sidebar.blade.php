99adbd06a0931a346de83a83f5884915aefe93a6
Mark John <markjohndatz@gmail.com>
2023-11-28 09:37:57 +0800

    Dashboard Update

---
+++ resources/views/layouts/admin-partials/_sidebar.blade.php
@@ -0,0 +1,21 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-solid fa-book"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">ID Admin</div>
+    </a>
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item {{ request()->is('admin') ? 'active' : '' }}">
+        <a class="nav-link" href="/admin">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+    
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+</ul>
